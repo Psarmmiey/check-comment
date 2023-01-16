cb29f96ac5cc0fff50ca304ea9c232e064aa9818
psarmmiey <psarmmiey@gmail.com>
2023-01-16 15:50:01 +0100

    fix: before making executable

--- main.go
+++ main.go
@@ -12,8 +12,16 @@ import (
 
 // CheckProject checks an entire project for controller functions with comments conforming to the Go Swaggo format.
 // It returns a map of function names to a boolean indicating whether the function has a comment conforming to the Go Swaggo format or not.
-func CheckProject(projectPath string) (map[string]bool, error) {
-	results := make(map[string]bool)
+func CheckProject(projectPath string) (map[string]struct {
+	Name    string
+	HasAll  bool
+	Missing []string
+}, error) {
+	results := make(map[string]struct {
+		Name    string
+		HasAll  bool
+		Missing []string
+	})
 
 	// Walk through the project directory
 	err := filepath.Walk(projectPath, func(path string, info os.FileInfo, err error) error {
@@ -21,8 +29,8 @@ func CheckProject(projectPath string) (map[string]bool, error) {
 			return err
 		}
 		if info.IsDir() {
-			fmt.Println("Checking directory: ", info.Name())
 			// If the directory is the "controller" folder
+			//	fmt.Println("Checking directory: ", info.Name(), " - ", path)
 			if info.Name() == "controllers" {
 				mainPath := filepath.Join(path, "main.go")
 				// check for main.go file in the controller folder
@@ -31,10 +39,8 @@ func CheckProject(projectPath string) (map[string]bool, error) {
 					if err != nil {
 						return err
 					}
-					for functionName, hasComment := range controllerFunctions {
-						if functionName != "Init" {
-							results[functionName] = hasComment
-						}
+					for functionName, funcInfo := range controllerFunctions {
+						results[functionName] = funcInfo
 					}
 				}
 			}
@@ -49,7 +55,11 @@ func CheckProject(projectPath string) (map[string]bool, error) {
 	return results, nil
 }
 
-func CheckControllerFunctions(filePath string) (map[string]bool, error) {
+func CheckControllerFunctions(filePath string) (map[string]struct {
+	Name    string
+	HasAll  bool
+	Missing []string
+}, error) {
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, filePath, nil, parser.ParseComments)
 	if err != nil {
@@ -57,7 +67,11 @@ func CheckControllerFunctions(filePath string) (map[string]bool, error) {
 	}
 
 	// Create a map to store the results
-	results := make(map[string]bool)
+	results := make(map[string]struct {
+		Name    string
+		HasAll  bool
+		Missing []string
+	})
 
 	// Check each function in the file
 	for _, decl := range f.Decls {
@@ -67,16 +81,52 @@ func CheckControllerFunctions(filePath string) (map[string]bool, error) {
 		}
 		// check if function name is Init
 		if fn.Name.Name != "Init" {
+			missingComments := []string{}
+			hasAll := true
 			// Check if the function has a comment
 			if fn.Doc != nil {
+				commentText := fn.Doc.Text()
 				// Check if the comment conforms to the Go Swaggo format
-				if strings.HasPrefix(fn.Doc.Text(), "// @Summary") {
-					results[fn.Name.Name] = true
-				} else {
-					results[fn.Name.Name] = false
+
+				// Check for specific comments
+				if !strings.Contains(commentText, "@summary") {
+					fmt.Println("Missing @summary")
+					missingComments = append(missingComments, "@summary")
+					hasAll = false
 				}
+				if !strings.Contains(commentText, "@description") {
+					missingComments = append(missingComments, "@description")
+					hasAll = false
+				}
+				if !strings.Contains(commentText, "@tags") {
+					missingComments = append(missingComments, "@tags")
+					hasAll = false
+				}
+				if !strings.Contains(commentText, "@accept") {
+					missingComments = append(missingComments, "@accept")
+					hasAll = false
+				}
+				if !strings.Contains(commentText, "@produce") {
+					missingComments = append(missingComments, "@produce")
+					hasAll = false
+				}
+				if !strings.Contains(commentText, "@router") {
+					missingComments = append(missingComments, "@router")
+					hasAll = false
+				}
+
 			} else {
-				results[fn.Name.Name] = false
+				missingComments = append(missingComments, "@summary", "@description", "@tags", "@accept", "@produce", "@router")
+				hasAll = false
+			}
+			results[fn.Name.Name] = struct {
+				Name    string
+				HasAll  bool
+				Missing []string
+			}{
+				Name:    fn.Name.Name,
+				HasAll:  hasAll,
+				Missing: missingComments,
 			}
 		}
 	}
@@ -85,13 +135,25 @@ func CheckControllerFunctions(filePath string) (map[string]bool, error) {
 
 func main() {
 	fmt.Println("Checking project...")
-	results, err := CheckProject("/Users/psarmmiey/development/dev/GOLA/blockchain-horizon-plus")
+
+	result, err := CheckProject("/Users/psarmmiey/development/dev/GOLA/blockchain-horizon-plus")
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
-	for fn, hasComment := range results {
-		fmt.Printf("Function %s: %t\n", fn, hasComment)
+	hasError := false
+	for _, v := range result {
+		if !v.HasAll {
+			fmt.Printf("Function %s: %t - missing %v\n", v.Name, v.HasAll, v.Missing)
+			hasError = true
+		}
 	}
+
+	if hasError {
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
+
 }
